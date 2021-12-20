049f840d667f6ba84af7376eaf9fe2a744777dc2
johnsonfoo <johnsonfoo18@gmail.com>
2021-12-20 23:02:43 +0800

    initial commit

---
+++ src/main/java/com/ntu/fyp/sdn6/Sdn6Application.java
@@ -0,0 +1,13 @@
+package com.ntu.fyp.sdn6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdn6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdn6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ntu/fyp/sdn6/Sdn6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntu.fyp.sdn6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdn6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
