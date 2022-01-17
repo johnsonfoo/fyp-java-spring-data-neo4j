51f3d4181b85d904d5dcbe5020fc801ad8a6d6dd
johnsonfoo <johnsonfoo18@gmail.com>
2022-01-18 00:24:05 +0800

    GH-1 - Remove RequestParam endpoint for MethodFQNController

--- src/main/java/com/ntu/fyp/sdn6/api/MethodFQNController.java
+++ src/main/java/com/ntu/fyp/sdn6/api/MethodFQNController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -35,8 +34,4 @@ class MethodFQNController {
     return methodFQNService.getCallers(calleeName);
   }
 
-  @GetMapping(path = "/")
-  List<MethodFQN> getDetails(@RequestParam("caller-name") String callerName){
-    return methodFQNService.getCallees(callerName);
-  }
 }
