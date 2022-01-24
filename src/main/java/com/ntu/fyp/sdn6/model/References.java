44ff76cc4805c1ab1718de10acbdd78466e38aa6
Johnson Foo <37036242+johnsonfoo@users.noreply.github.com>
2022-01-25 00:56:21 +0800

    GH-9 - Create References relationship

---
+++ src/main/java/com/ntu/fyp/sdn6/model/References.java
@@ -0,0 +1,36 @@
+package com.ntu.fyp.sdn6.model;
+
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.RelationshipProperties;
+import org.springframework.data.neo4j.core.schema.TargetNode;
+
+/**
+ * This class represents the REFERENCES relationship. A REFERENCES relationship is a directed edge
+ * from a FQN node to a FQN node with property version.
+ */
+@RelationshipProperties
+public class References {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @TargetNode
+  private final FQN referencedFQN;
+
+  private final String version;
+
+  public References(FQN referencedFQN, String version) {
+    this.referencedFQN = referencedFQN;
+    this.version = version;
+  }
+
+  public String getVersion() {
+    return version;
+  }
+
+  public FQN getReferencedFQN() {
+    return referencedFQN;
+  }
+}
