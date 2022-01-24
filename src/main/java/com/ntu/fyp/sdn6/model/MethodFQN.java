87ab38b968124dcd561f1a89568e1e20d0151e56
Johnson Foo <37036242+johnsonfoo@users.noreply.github.com>
2022-01-24 17:14:37 +0800

    GH-7 - Modify MethodFQN class to extend from FQN class

--- src/main/java/com/ntu/fyp/sdn6/model/MethodFQN.java
+++ src/main/java/com/ntu/fyp/sdn6/model/MethodFQN.java
@@ -1,7 +1,5 @@
 package com.ntu.fyp.sdn6.model;
 
-import org.springframework.data.neo4j.core.schema.GeneratedValue;
-import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.schema.Relationship;
 
@@ -10,26 +8,17 @@ import java.util.List;
 
 /**
  * This class represents the MethodFQN node. MethodFQN nodes all have property name. Each MethodFQN
- * caller node CALLS 0 or more MethodFQN callee nodes.
+ * node inherits the relationships Contains, References, Inserts, Updates, Deletes from parent FQN
+ * and have relationship Calls.
  */
 @Node
-public class MethodFQN {
-
-  @Id
-  @GeneratedValue
-  private Long id;
-
-  private String name;
+public class MethodFQN extends FQN {
 
   @Relationship(type = "CALLS", direction = Relationship.Direction.OUTGOING)
   private List<Calls> callees = new ArrayList<>();
 
   public MethodFQN(String name) {
-    this.name = name;
-  }
-
-  public String getName() {
-    return name;
+    super(name);
   }
 
   public List<Calls> getCallees() {
