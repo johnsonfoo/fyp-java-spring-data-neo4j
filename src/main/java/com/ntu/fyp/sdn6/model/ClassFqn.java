3d4ff9bc03a5a1e480fe17bf6336633f3f46fb7d
johnsonfoo <johnsonfoo18@gmail.com>
2022-01-25 23:46:06 +0800

    GH-34 - Add Inherits relationship property and getter to ClassFqn class

--- src/main/java/com/ntu/fyp/sdn6/model/ClassFqn.java
+++ src/main/java/com/ntu/fyp/sdn6/model/ClassFqn.java
@@ -1,6 +1,10 @@
 package com.ntu.fyp.sdn6.model;
 
 import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class represents the ClassFqn node. ClassFqn nodes all have property name. Each ClassFqn
@@ -14,6 +18,9 @@ public class ClassFqn extends Fqn {
 
   boolean isTest;
 
+  @Relationship(type = "INHERITS", direction = Relationship.Direction.OUTGOING)
+  private List<Inherits> inheritedFqns = new ArrayList<>();
+
   public ClassFqn(String name, boolean isAbstract, boolean isTest) {
     super(name);
     this.isAbstract = isAbstract;
@@ -26,5 +33,9 @@ public class ClassFqn extends Fqn {
 
   public boolean isTest() {
     return isTest;
+  }
+
+  public List<Inherits> getInheritedFqns() {
+    return inheritedFqns;
   }
 }
