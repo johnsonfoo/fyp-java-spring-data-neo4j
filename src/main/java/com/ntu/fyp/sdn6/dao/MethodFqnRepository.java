098c1b962ef7728ebbcbed6bbedbe7cd302eedf3
johnsonfoo <johnsonfoo18@gmail.com>
2022-01-26 23:48:37 +0800

    GH-49 - Update MethodFqnRepository to extend FqnBaseRepository

--- src/main/java/com/ntu/fyp/sdn6/dao/MethodFqnRepository.java
+++ src/main/java/com/ntu/fyp/sdn6/dao/MethodFqnRepository.java
@@ -1,16 +1,11 @@
 package com.ntu.fyp.sdn6.dao;
 
 import com.ntu.fyp.sdn6.model.MethodFqn;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface MethodFqnRepository extends Neo4jRepository<MethodFqn, Long> {
-
-  List<MethodFqn> findAll();
-
-  List<MethodFqn> findByName(@Param("name") String name);
+public interface MethodFqnRepository extends FqnBaseRepository<MethodFqn> {
 
   List<MethodFqn> findByCalledMethodFqnsCalledMethodFqnName(@Param("name") String name);
 }
