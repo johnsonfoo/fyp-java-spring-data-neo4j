6818c5f8b459478b6fcc03864fc8a32d77b833d7
johnsonfoo <johnsonfoo18@gmail.com>
2022-01-27 19:46:28 +0800

    GH-66 - Create REST endpoint to get ClassFqns that Inherits a specific ClassFqn

--- src/main/java/com/ntu/fyp/sdn6/api/ClassFqnController.java
+++ src/main/java/com/ntu/fyp/sdn6/api/ClassFqnController.java
@@ -2,13 +2,18 @@ package com.ntu.fyp.sdn6.api;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+import com.ntu.fyp.sdn6.model.ClassFqn;
 import com.ntu.fyp.sdn6.service.ClassFqnService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(path = "/api/v0/class-fqn")
+@RequestMapping(path = "/api/v0/class-fqns")
 @Tag(name = "class-fqn")
 class ClassFqnController {
 
@@ -17,5 +22,10 @@ class ClassFqnController {
   @Autowired
   ClassFqnController(ClassFqnService classFqnService) {
     this.classFqnService = classFqnService;
+  }
+
+  @GetMapping(path = "/inherits/search")
+  List<ClassFqn> getInheritedFqn(@RequestParam("name") String name) {
+    return classFqnService.findByInheritedName(name);
   }
 }
--- src/main/java/com/ntu/fyp/sdn6/dao/ClassFqnRepository.java
+++ src/main/java/com/ntu/fyp/sdn6/dao/ClassFqnRepository.java
@@ -1,7 +1,12 @@
 package com.ntu.fyp.sdn6.dao;
 
 import com.ntu.fyp.sdn6.model.ClassFqn;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ClassFqnRepository extends FqnBaseRepository<ClassFqn> {
+
+  List<ClassFqn> findByInheritedClassFqnsInheritedClassFqnName(@Param("name") String name);
 
 }
