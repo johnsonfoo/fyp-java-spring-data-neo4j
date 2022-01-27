d873375fb5f208306415bb43aa1596d70f2eb1ca
johnsonfoo <johnsonfoo18@gmail.com>
2022-01-27 16:02:09 +0800

    GH-72 - Create REST endpoint to get a specific Fqn

--- src/main/java/com/ntu/fyp/sdn6/api/FqnController.java
+++ src/main/java/com/ntu/fyp/sdn6/api/FqnController.java
@@ -2,13 +2,16 @@ package com.ntu.fyp.sdn6.api;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+import com.ntu.fyp.sdn6.model.Fqn;
 import com.ntu.fyp.sdn6.service.FqnService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api/v0/fqn")
+@RequestMapping(path = "/api/v0/fqns")
 @Tag(name = "fqn")
 class FqnController {
 
@@ -17,5 +20,10 @@ class FqnController {
   @Autowired
   FqnController(FqnService fqnService) {
     this.fqnService = fqnService;
+  }
+
+  @GetMapping(path = "/search")
+  Fqn getFqn(@RequestParam("name") String name) {
+    return fqnService.findByName(name);
   }
 }
