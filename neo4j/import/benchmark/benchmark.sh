5739af8bd1480e99b7de24ae6e7667a208d8671f
johnsonfoo <johnsonfoo18@gmail.com>
2022-02-18 10:45:55 +0800

    GH-112 - Update benchmark shell script to create results directory and print query time

--- neo4j/import/benchmark/benchmark.sh
+++ neo4j/import/benchmark/benchmark.sh
@@ -1,2 +1,6 @@
+[ -d $NEO4J_HOME/import/benchmark/results/ ] || mkdir $NEO4J_HOME/import/benchmark/results/
+
 cat $NEO4J_HOME/import/benchmark/benchmark.cypher |
-  bin/cypher-shell -u neo4j -p test --format plain >$NEO4J_HOME/import/benchmark/results/benchmark.txt
+  bin/cypher-shell -u neo4j -p test --format plain |
+  tee $NEO4J_HOME/import/benchmark/results/benchmark.txt |
+  grep "Time: "
