5a468e966fabb41a01ff83520de62f6d8bf5b7a5
Johnson Foo <37036242+johnsonfoo@users.noreply.github.com>
2022-03-13 15:43:14 +0800

    GH-130 - Refactor benchmark script by extracting variables and adding comments

--- neo4j/import/benchmark/benchmark.sh
+++ neo4j/import/benchmark/benchmark.sh
@@ -1,6 +1,22 @@
-[ -d $NEO4J_HOME/import/benchmark/results/ ] || mkdir $NEO4J_HOME/import/benchmark/results/
+# Change the below variables only
+n=10
+input_file=$NEO4J_HOME/import/benchmark/find_at_least_n_callee.cypher
 
-cat $NEO4J_HOME/import/benchmark/benchmark.cypher |
-  bin/cypher-shell -u neo4j -p test --format plain |
-  tee $NEO4J_HOME/import/benchmark/results/benchmark.txt |
-  grep "Time: "
+# Constants
+output_directory=$NEO4J_HOME/import/benchmark/results/
+output_file=$NEO4J_HOME/import/benchmark/results/benchmark.txt
+
+# Creates output_directory if it does not exist
+[ -d $output_directory ] || mkdir $output_directory
+
+# Empties the output_file
+>$output_file
+
+# Performs the query defined in input_file for N times
+# Each query execution time is appended to output_file
+for ((i = 1; i <= n; i++)); do
+  cat $input_file |
+    bin/cypher-shell -u neo4j -p test --format plain |
+    grep "Time: " | sed 's/^.*: //' |
+    tee -a $output_file
+done
